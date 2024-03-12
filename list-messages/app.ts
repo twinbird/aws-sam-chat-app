7cc36f81df0798ef8ef6a969231ea24c6cc120fb
twinbird <ixa2063@gmail.com>
2024-03-12 18:46:31 +0900

    追加した関数へDynamoDBから読み出しするための処理を加えた

--- list-messages/app.ts
+++ list-messages/app.ts
@@ -1,18 +1,26 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
 
 interface Response {
   statusCode: number;
   body: string;
 }
 
+interface Post {
+  createdAt: string;
+  message: string;
+}
+
 /**
  * API成功時のレスポンスを返す
  */
-const successResponse = (): Response => {
+const successResponse = (posts: [Post]): Response => {
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: 'success',
+      posts: posts,
     }),
   };
 };
@@ -30,6 +38,25 @@ const errorResponse = (): Response => {
 };
 
 /**
+ * メッセージをデータベースから取り出す
+ */
+const fetchPosts = async (): [Post] => {
+  const client = new DynamoDBClient({});
+  const docClient = DynamoDBDocumentClient.from(client);
+  const command = new ScanCommand({
+    TableName: 'ChatTable',
+  });
+  const data = await docClient.send(command);
+  console.log(data);
+  return data.Items.map((m) => {
+    return {
+      createdAt: m.createdAt,
+      message: m.message,
+    };
+  });
+}
+
+/**
  * メッセージ取得API
  *
  * Request Bodyに以下の形式のJSONが含まれることを期待します。
@@ -37,7 +64,8 @@ const errorResponse = (): Response => {
  */
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
-    return successResponse();
+    const posts = await fetchPosts();
+    return successResponse(posts);
   } catch (err) {
     console.log(err);
     return errorResponse();
